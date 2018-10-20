ce1046fa9f9f86371a689bbc0633ed336fa976c1
dpeeps <ds016524dp@dsisdconnect.com>
2018-10-19 22:10:46 -0500

    Added license statement and adding to master Branch, not final state.

--- RasPi_Code/RasPi/src/RasPi_Runner.java
+++ RasPi_Code/RasPi/src/RasPi_Runner.java
@@ -1,3 +1,12 @@
+// Copyright (C) 2018 David Pieper
+// Code at this point prints out hello world but will at later points run actions or RaspberryPi
+/*This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program. If not, see https://www.gnu.org/licenses/.
+*/
 public class RasPi_Runner
 {
     public static void main(String args[])
